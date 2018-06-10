7b94364c19dbb4c0e9d1230d7f286661f086b152
marusack <marizz_05@hotmail.com>
2018-06-10 14:07:11 -0300

    documentations and first steps to writ on db

--- app/src/main/java/com/example/braca/muffinapplication/MainActivity.java
+++ app/src/main/java/com/example/braca/muffinapplication/MainActivity.java
@@ -1,14 +1,61 @@
 package com.example.braca.muffinapplication;
 
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
+
+    private StorageReference mStorageRef;
+    private DatabaseReference dbReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //comment
+        //todo must have authentification PR first in this case will be just write over users
+
+        //firt setp to get all the element from the view
+        EditText Name = (EditText) findViewById(R.id.textBox_Name);
+        EditText Lastname = (EditText) findViewById(R.id.textBox_lastName);
+        EditText cellphone = (EditText) findViewById(R.id.textBox_cellphone);
+
+        //second step to get the actual data from the elements
+        String nameString = Name.getText().toString();
+        String lastnameString = Name.getText().toString();
+        String cellphoneString = Name.getText().toString();
+
+        //second step bis? to get the button and define the listener
+        Button goSave = (Button) findViewById(R.id.btn_save);
+
+        //third step define the reference to db (the root of the json doc)
+        dbReference = FirebaseDatabase.getInstance().getReference();
+
+        goSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+
+
     }
+
+
 }
--- app/src/main/java/objects/User.java
+++ app/src/main/java/objects/User.java
@@ -1,4 +1,51 @@
 package objects;
 
 public class User {
+
+    //clase, define comportamiento los atributos (molde a seguir)
+
+    private String firstName;
+    private String lastName;
+    private Integer cellphone; //atributos
+
+
+    public void User(){} //genero entidades unicas ()
+
+    public void User(String firstName, String lastName, Integer cellphone){
+        User user = new User();
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        user.setCellphone(cellphone);
+    }
+
+    public void User(String firstName, Integer cellphone){
+        User user = new User();
+        user.setFirstName(firstName);
+        user.setLastName(""); //no null
+        user.setCellphone(cellphone);
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getCellphone() {
+        return cellphone;
+    }
+
+    public void setCellphone(Integer cellphone) {
+        this.cellphone = cellphone;
+    }
 }
