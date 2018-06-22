6a6201a9e34f3e8d5546357ab9e52341dd5659c6
marusack <marielsack@gmail.com>
2018-06-21 22:41:04 -0300

    Fix on User classes and integration with Database (#6)
    * module for saving users fixed
    
    * creating listener in order to erase the default data when they are touched
    
    * creating listener in order to erase the default data when they are touched V2
    
    * comments addressed

--- app/src/main/java/com/example/braca/muffinapplication/MainActivity.java
+++ app/src/main/java/com/example/braca/muffinapplication/MainActivity.java
@@ -8,67 +8,68 @@ import android.widget.EditText;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.storage.StorageReference;
-
-import java.io.File;
-import java.util.HashMap;
 
 import objects.User;
 
+
 public class MainActivity extends AppCompatActivity {
 
-    private StorageReference mStorageRef;
     private DatabaseReference dbReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //todo must have authentification PR first in this case will be just write over users
 
-        //firt setp to get all the element from the view
-        EditText Name = (EditText) findViewById(R.id.textBox_Name);
-        EditText Lastname = (EditText) findViewById(R.id.textBox_lastName);
-        EditText cellphone = (EditText) findViewById(R.id.textBox_cellphone);
-
-        //second step to get the actual data from the elements
-        String nameString = Name.getText().toString().trim();
-        String lastnameString = Lastname.getText().toString().trim();
-        Integer cellphoneInt = Integer.parseInt( cellphone.getText().toString().trim());
-
-        //second step bis? to get the button and define the listener
-        Button goSave = (Button) findViewById(R.id.btn_save);
-
-        final User firstU = new User(nameString , lastnameString , cellphoneInt);
-
-        //third step define the reference to db (the root of the json doc)
         dbReference = FirebaseDatabase.getInstance().getReference();
+        //first step to get all the element from the view
+        final EditText name = findViewById(R.id.textBox_Name);
+        final EditText lastName = findViewById(R.id.textBox_lastName);
+        final EditText cellphone = findViewById(R.id.textBox_cellphone);
+
+        name.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (name.getText().toString().equals("Name")){
+                    name.setText("");
+                }
+            }
+        });
+
+        lastName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (lastName.getText().toString().equals("Last Name")){
+                    lastName.setText("");
+                }
+            }
+        });
+
+        cellphone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(cellphone.getText().toString().equals("Cellphone")){
+                    cellphone.setText("");
+                }
+            }
+        });
+
+        Button goSave = findViewById(R.id.btn_save);
 
         goSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-            //steps to store in the data base
-                //1- create a child  in the root object (remember to show the json from firebase)
-                //todo check if this workaround works on db
-                dbReference.child(firstU.getUserId().toString());//create a unique child for each time
-               //dbReference.push().child(firstU.getUserId().toString());
+                //todo must have authentication PR first in this case will be just write over users
 
+                //second step to get the actual data from the elements
+                String nameString = name.getText().toString().trim();
+                String lastNameString = lastName.getText().toString().trim();
+                String cellphoneString = cellphone.getText().toString().trim();
+                User user = new User(nameString, lastNameString, cellphoneString);
 
-                HashMap<String, String> datamap = new HashMap<String, String>();
-                datamap.put("FirstName", firstU.getFirstName());
-                datamap.put("LastName", firstU.getLastName());
-                datamap.put("PhoneNumer", firstU.getCellphone().toString());
-                //2- assign the values to store
-
-                //todo test the  firstU as object
-                dbReference.push().setValue(datamap);//I must try set the object as value
+                dbReference.child(user.getId());
+                dbReference.push().setValue(user);
             }
         });
-
-
-
-
     }
-
-
 }
